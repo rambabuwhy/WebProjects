5928695c7319e35da6939fe5a2ecb2f2e7107e4c
Rambabu Yerajana <rambabuwhy@gmail.com>
2016-10-09 20:00:26 +0530

    Add files via upload

---
+++ Udacity_AJAX_INIT/minicourse-ajax-project/js/script.js
@@ -0,0 +1,28 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+	
+	var streetStr = $('#street').val();
+	var cityStr = $('#city').val();
+	var address = streetStr + ','+ cityStr;
+	$greeting.text('So you want live at ' +  address + '?');
+	
+	var streeviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' +  address + '';
+	$body.append('<img class="bgimg" src="' + streeviewUrl + '">'); 
+    return false;
+};
+
+$('#form-container').submit(loadData);
